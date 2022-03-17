b1c8b9ec329784d86e3c582fd41cde4d44f5206c
Mary Hagan Chandler <mary_hagan1@baylor.edu>
2022-03-16 20:52:35 -0500

    Finished sitka_4

--- sitka_4.py
+++ sitka_4.py
@@ -1,3 +1,112 @@
+# Copied sitka_2 to start sitka_3
+
+#Objectives for sitka_3
+# 1) Changing the file to include all the data for the year of 2018
+# 2) Changing the title to Daily high and low temperatures - 2018
+# 3) Extracting the low temperatures from the file and adding to the chart
+# 4) Shading in the area between high and low
+
+
+
+import csv
+from datetime import datetime
+
+#Create a file object.
+open_file = open("death_valley_2018_simple.csv","r")
+
+#Create a CSV file.
+csv_file = csv.reader(open_file, delimiter = ",")
+
+#Skip the first row.
+header_row = next(csv_file)
+print(header_row)
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+
+
+highs = []
+lows = []
+dates = []
+
+#Extract all the max temperatures from the file and add them to the list.
+for row in csv_file:
+
+    try:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        high = int(row[4])
+        low = int(row[5])
+        
+    except ValueError:
+        print(f"Missing data for {current_date}")
+
+    else:    
+        highs.append(high)
+        lows.append(low)
+        dates.append(current_date)      #Can do this line and above in one line
+
+#Print out the lists.
+print(highs)
+print(dates)
+
+
+
+
+import matplotlib.pyplot as plt #pyplot is usually referred to as plt
+
+fig = plt.figure()      #Created this so we can format the date better
+
+plt.plot(dates, highs, c = "red")
+plt.plot(dates, lows, c = "blue")
+
+plt.fill_between(dates, highs, lows, facecolor = "blue", alpha = 0.1)
+    #Give this function one x and two y's
+    #Alpha is the transparency; alpha ranges from 1 to 0, where 0 is
+    #   the most transparent and 1 is not transparent
+
+plt.title("Daily high and low temperatures - 2018",fontsize = 16)
+plt.xlabel("Month of July 2018")
+plt.ylabel("Temperatures (F)",fontsize=16)
+
+plt.tick_params(axis = "both",which="major",labelsize=16)
+    #Tick paramaters are the ticks that are on the graph, we want to show
+    #   them on both the x and y axis, and we want to show the major tick
+    #   marks.
+
+fig.autofmt_xdate()     #Makes the dates slanted so we can see the whole thing
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
 #Supposed to copy and paste from sitka_3, but Mary typed it out
 
 import csv
@@ -47,3 +156,5 @@ fig = plt.figure()
 
 #More here
 
+
+"""
